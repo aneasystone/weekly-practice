23d0aca2bc09b90f3adbfbe4e801550f2393112d
aneasystone <aneasystone@gmail.com>
2023-12-20 07:42:29 +0800

    [WEEK050] Finish the part one of Java 21 notes

---
+++ notes/week050-java-21-notes-2/demo/UnnamedVariables.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class UnnamedVariables {
+
+    record Person(String name, Integer age) {
+    }
+
+    public static void main(String[] args) {
+        test_unnamed_variables();
+        test_unnamed_exception_variables("xx");
+        test_unnamed_exception_variables("123");
+        test_unnamed_patterns(new Person("Zhangsan", 18));
+    }
+
+    private static void test_unnamed_exception_variables(String s) {
+        try { 
+            int i = Integer.parseInt(s);
+            System.out.println("Good number: " + i);
+        } catch (NumberFormatException _) { 
+            System.out.println("Bad number: " + s);
+        }
+    }
+
+    private static void test_unnamed_variables() {
+        int index = 0;
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        for (Integer _ : list) {
+            System.out.println(index++);
+        }
+    }
+
+    private static void test_unnamed_patterns(Object obj) {
+        if (obj instanceof Person(String name, Integer _)) {
+            System.out.println("Name: " + name);
+        }
+    }
+}
