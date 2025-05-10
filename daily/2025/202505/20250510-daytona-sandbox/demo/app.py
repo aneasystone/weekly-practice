f8a2b5c41fc364612d9ccb9ded2555c4bb513691
aneasystone <aneasystone@gmail.com>
2025-05-10 14:40:12 +0800

    [DAILY] Daytona Sandbox Management

---
+++ daily/2025/202505/20250510-daytona-sandbox/demo/app.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+DAYTONA_API_KEY = os.getenv("DAYTONA_API_KEY")
+
+# ------------------------------------------------
+
+from daytona_sdk import Daytona, DaytonaConfig, CreateSandboxParams, CodeRunParams, SandboxResources
+
+# Initialize the Daytona client
+daytona = Daytona(DaytonaConfig(api_key=DAYTONA_API_KEY))
+
+# Create the Sandbox instance
+params = CreateSandboxParams(
+    language="python",
+    image="debian:12.9",
+    env_vars={"DEBUG": "true"},
+    resources=SandboxResources(cpu=2, memory=4),
+    auto_stop_interval=0
+)
+sandbox = daytona.create(params)
+
+# Run code securely inside the Sandbox
+response = sandbox.process.code_run('''
+import os
+print("环境变量：DEBUG = ", os.getenv("DEBUG"))
+''', params=params)
+if response.exit_code != 0:
+    print(f"Error running code: {response.exit_code} {response.result}")
+else:
+    print(response.result)
+
+# Clean up the Sandbox
+daytona.remove(sandbox)
